f156d138cceefc11b146273a89e2061c589491ee
Swpanil <swapnil00patil@gmail.com>
2018-05-14 21:52:36 +0800

    Added mock for axios..moved app.test under__test__

---
+++ src/__test__/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from '../App'
+
+describe('App', () => {
+  it('App should be defined', () => {
+    expect(App).toBeDefined()
+  })
+})
